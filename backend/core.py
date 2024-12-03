22af602c87ba9cbba595b175b902f7be8ae24ef6
Radosław Micał <micalradoslaw@gmail.com>
2024-12-03 23:13:42 +0100

    added history of conversation

--- backend/core.py
+++ backend/core.py
@@ -1,4 +1,7 @@
+from typing import List, Dict, Any
+
 from dotenv import load_dotenv
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -10,7 +13,8 @@ load_dotenv()
 
 INDEX_NAME = "langchain-doc-index"
 
-def run_llm(query: str):
+
+def run_llm(query: str, chat_history: List[Dict[str, Any]]):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
     chat = ChatOpenAI(verbose=True, temperature=0)
@@ -19,20 +23,24 @@ def run_llm(query: str):
 
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
 
-    qa = create_retrieval_chain(
-        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
-
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
     )
 
-    result = qa.invoke(input={"input": query})
+    qa = create_retrieval_chain(
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
+    )
+
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
     parsed_result = {
         "query": result["input"],
         "result": result["answer"],
-        "source_documents": result["context"]
+        "source_documents": result["context"],
     }
     return parsed_result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     res = run_llm(query="What is LangChain Chain")
     print(res["result"])
